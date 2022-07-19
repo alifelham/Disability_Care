6f8ca6565c61d300ab1408a0a2436a0d0637ee36
Alif Elham <alifelham@gmail.com>
2022-07-19 11:28:36 +0600

    Created RESTFUL Patient class which sends JSON response of patient attributes to API layer upon user login.

--- healthcare_app/src/main/java/healthcare/healthcare_app/HealthcareAppApplication.java
+++ healthcare_app/src/main/java/healthcare/healthcare_app/HealthcareAppApplication.java
@@ -1,13 +1,36 @@
 package healthcare.healthcare_app;
 
+import healthcare.healthcare_app.patient.Patient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class HealthcareAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HealthcareAppApplication.class, args);
 	}
 
+	@GetMapping
+	public List<Patient> echo() {
+		return List.of(
+				new Patient(
+					1L,
+					"someone",
+					"someone@gmail.com",
+					LocalDate.of(1990, Month.JULY, 01),
+					32
+
+			)
+		);
+	}
+
 }
+
