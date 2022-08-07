978e5123472e9a258cd4c7b6ba71cf86cebec9f3
Junayed Hasan <junayedhasan100@gmail.com>
2022-08-08 00:08:29 +0600

    Completed user authentication and authorization using spring security, Jwt tokens, cookies and connected backend with firestore database.

---
+++ healthcare_app/src/main/java/com/example/healthcare/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.example.healthcare.controller;
+
+import com.example.healthcare.exception.ResourceNotFoundException;
+import com.example.healthcare.model.User;
+import com.example.healthcare.repository.UserRepository;
+import com.example.healthcare.security.CurrentUser;
+import com.example.healthcare.security.UserPrincipal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.ExecutionException;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/user/me")
+    @PreAuthorize("hasRole('USER')")
+    public User getCurrentUser(@CurrentUser UserPrincipal userPrincipal) throws ExecutionException, InterruptedException {
+        return userRepository.findById(userPrincipal.getId());
+    }
+}
---
+++ healthcare_app/src/main/java/com/example/healthcare/security/oauth2/CustomOAuth2UserService.java
@@ -0,0 +1,91 @@
+package com.example.healthcare.security.oauth2;
+
+import com.example.healthcare.exception.OAuth2AuthenticationProcessingException;
+import com.example.healthcare.model.AuthProvider;
+import com.example.healthcare.model.User;
+import com.example.healthcare.repository.UserRepository;
+import com.example.healthcare.security.UserPrincipal;
+import com.example.healthcare.security.oauth2.user.OAuth2UserInfo;
+import com.example.healthcare.security.oauth2.user.OAuth2UserInfoFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+
+@Service
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest oAuth2UserRequest) throws OAuth2AuthenticationException {
+        System.out.println("oAuth2UserRequest => " + oAuth2UserRequest.getClientRegistration().getClientName());
+
+        OAuth2User oAuth2User = super.loadUser(oAuth2UserRequest);
+
+        System.out.println("oAuth2 User Req => " + oAuth2UserRequest.getClientRegistration().getClientName());
+
+        System.out.println(oAuth2User);
+        try {
+            return processOAuth2User(oAuth2UserRequest, oAuth2User);
+        } catch (AuthenticationException ex) {
+            throw ex;
+        } catch (Exception ex) {
+            // Throwing an instance of AuthenticationException will trigger the OAuth2AuthenticationFailureHandler
+            throw new InternalAuthenticationServiceException(ex.getMessage(), ex.getCause());
+        }
+    }
+
+    private OAuth2User processOAuth2User(OAuth2UserRequest oAuth2UserRequest, OAuth2User oAuth2User) throws ExecutionException, InterruptedException {
+        OAuth2UserInfo oAuth2UserInfo = OAuth2UserInfoFactory.getOAuth2UserInfo(oAuth2UserRequest.getClientRegistration().getRegistrationId(), oAuth2User.getAttributes());
+        if(StringUtils.isEmpty(oAuth2UserInfo.getEmail())) {
+            throw new OAuth2AuthenticationProcessingException("Email not found from OAuth2 provider");
+        }
+        System.out.println("In process OAuth2User" + oAuth2UserInfo.getEmail());
+        User user = userRepository.findByEmail(oAuth2UserInfo.getEmail());
+
+        System.out.println("User in process" + user);
+        if(user.getEmail() != null) {
+            if(!user.getProvider().equals(AuthProvider.valueOf(oAuth2UserRequest.getClientRegistration().getRegistrationId()))) {
+                throw new OAuth2AuthenticationProcessingException("Looks like you're signed up with " +
+                        user.getProvider() + " account. Please use your " + user.getProvider() +
+                        " account to login.");
+            }
+            user = updateExistingUser(user, oAuth2UserInfo);
+        } else {
+            user = registerNewUser(oAuth2UserRequest, oAuth2UserInfo);
+        }
+
+        System.out.println("In if ==> " + user.getEmail());
+        return UserPrincipal.create(user, oAuth2User.getAttributes());
+    }
+
+    private User registerNewUser(OAuth2UserRequest oAuth2UserRequest, OAuth2UserInfo oAuth2UserInfo) throws ExecutionException, InterruptedException {
+        User user = new User();
+
+        System.out.println("In register new user");
+
+        user.setProvider(AuthProvider.valueOf(oAuth2UserRequest.getClientRegistration().getRegistrationId()));
+        user.setProviderId(oAuth2UserInfo.getId());
+        user.setName(oAuth2UserInfo.getName());
+        user.setEmail(oAuth2UserInfo.getEmail());
+        user.setImageUrl(oAuth2UserInfo.getImageUrl());
+        return userRepository.savee(user);
+    }
+
+    private User updateExistingUser(User existingUser, OAuth2UserInfo oAuth2UserInfo) throws ExecutionException, InterruptedException {
+        existingUser.setName(oAuth2UserInfo.getName());
+        existingUser.setImageUrl(oAuth2UserInfo.getImageUrl());
+        return userRepository.savee(existingUser);
+    }
+
+}
