afb74ce3072b2ce3e08a2b11da348fdc7d76dee6
alifelham <alifelham@gmail.com>
2022-08-08 23:59:49 +0600

    Converted signup.jsx file to javascript file.

---
+++ healthcare_app/hcare-app/src/user/signup/Signup.js
@@ -0,0 +1,131 @@
+import React, { Component } from 'react';
+import { Link, Redirect } from 'react-router-dom'
+import { GOOGLE_AUTH_URL, FACEBOOK_AUTH_URL } from '../../constants';
+import { signup } from '../../util/APIUtils';
+import Alert from 'react-s-alert';
+import './signupp.css'
+
+class Signup extends Component {
+    render() {
+        if (this.props.authenticated) {
+            return <Redirect
+                to={{
+                    pathname: "/",
+                    state: { from: this.props.location }
+                }} />;
+        }
+
+        return (
+            <div className='bg'>
+                <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css'></link>
+                <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css' />
+                <link rel='stylesheet' href='./Signupp.css' />
+                <script src='https://cdnjs.cloudflare.com/ajax/libs/react/16.12.0/umd/react.production.min.js'></script>
+                <script src='https://cdnjs.cloudflare.com/ajax/libs/react-dom/16.11.0/umd/react-dom.production.min.js'></script><script src="./script.js"></script>
+                
+                <div className='container'><SignupForm {...this.props} /></div>
+            </div>
+
+
+        );
+    }
+}
+
+class SignupForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            name: '',
+            password: '',
+            confirm_pass: ''
+        }
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const inputName = target.name;
+        const inputValue = target.value;
+
+        this.setState({
+            [inputName]: inputValue
+        });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+
+        const signUpRequest = Object.assign({}, this.state);
+
+        signup(signUpRequest)
+            .then(response => {
+                Alert.success("You're successfully registered. Please login to continue!");
+                this.props.history.push("/login");
+            }).catch(error => {
+                console.log("Here" + error + error.message)
+                Alert.error((error && error.message) || 'The passwords dont\'t match! Please try again.');
+            });
+    }
+
+    render() {
+        return (
+            <div className='form-box'>
+                
+                <div className="header-form">
+                    <h4 className="text-center"><i style={{ color: "white", fontSize: "35px" }}>REGISTER</i></h4>
+                    <div className="image">
+                    </div>
+                </div>
+
+                <div className="body-form">
+
+                <form onSubmit={this.handleSubmit}>
+                    <div className="input-group mb-3">
+                        <div className="input-group-prepend">
+                            <span className="input-group-text"><i className="fa fa-user"></i></span>
+                        </div>
+                        <input name = "email" type="email" className="form-control" placeholder="Email" value={this.state.email} onChange={this.handleInputChange} />
+                    </div>
+                    <div className="input-group mb-3">
+                        <div className="input-group-prepend">
+                            <span className="input-group-text"><i className="fa fa-user"></i></span>
+                        </div>
+                        <input name = "name" type="name" className="form-control" placeholder="Full Name" value={this.state.name} onChange={this.handleInputChange} />
+                    </div>
+                    <div className="input-group mb-3">
+                        <div className="input-group-prepend">
+                            <span className="input-group-text"><i className="fa fa-lock"></i></span>
+                        </div>
+                        <input name = "password" type="password" className="form-control" placeholder="Password" value={this.state.password} onChange={this.handleInputChange} />
+                    </div>
+                    <div className="input-group mb-3">
+                        <div className="input-group-prepend">
+                            <span className="input-group-text"><i className="fa fa-lock"></i></span>
+                        </div>
+                        <input name = "confirm_pass" type="password" className="form-control" placeholder="Confirm Password" value={this.state.confirm_pass} onChange={this.handleInputChange} />
+                    </div>
+                    <div className="SignupButton">
+                        <button type="submit" className="btn btn-secondary btn-block">SIGNUP</button>
+                    </div>
+
+                    <div className="message">
+                        <div><a href="/login">Already have an account? LOGIN</a>
+                        </div>
+                    </div>
+                    <div className='or'>
+                        <a>------------ OR USE ------------</a>
+                    </div>
+                    <div className="social">
+                        <a href={FACEBOOK_AUTH_URL}><i className="fab fa-facebook"></i></a>
+                        <a href={GOOGLE_AUTH_URL}><i className="fab fa-google"></i></a>
+                    </div>
+                </form>
+            </div>
+            </div>
+        );
+    }
+}
+
+export default Signup
